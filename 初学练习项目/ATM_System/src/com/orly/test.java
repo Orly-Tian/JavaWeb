cd41fcbf85dbe57d7d54d095d26c9c24d7892796
Orly <76263061+Orly-Tian@users.noreply.github.com>
2024-09-28 23:31:54 +0800

    Create test.java
    接口测试文件。

---
+++ 初学练习项目/ATM_System/src/com/orly/test.java
@@ -0,0 +1,10 @@
+package com.orly;
+
+
+public class test {
+    public static void main(String[] args) {
+        // 创建一个ATM对象，测试start方法
+        ATM atm = new ATM();
+        atm.start();
+    }
+}
